0437d12d9f22e612529db82c722ae96235e5671f
Giritharan_2409 <giritharand3@gmail.com>
2026-03-20 14:29:20 +0530

    updated quick_seed.py

--- backend/quick_seed.py
+++ backend/quick_seed.py
@@ -6,7 +6,7 @@ dotenv_path = os.path.join(os.path.dirname(__file__), 'backend', '.env')
 load_dotenv(dotenv_path)
 
 def seed():
-    uri = "mongodb+srv://giritharand3_db_user:cms@cms.sufjn3m.mongodb.net/?appName=CMS"
+    uri = "mongodb+srv://priyadharshini:Ezhilithanya@cluster0.crvutrr.mongodb.net/College_db"
     print(f"Connecting to Atlas...")
     client = MongoClient(uri, serverSelectionTimeoutMS=30000)
     db = client["cms"]
